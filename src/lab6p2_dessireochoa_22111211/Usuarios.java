f4e96a01cd0cea7146d4cc61184a52a80b175682
Dessi <dessireochoa12@gmail.com>
2021-11-19 13:17:55 -0600

    primer envio

---
+++ src/lab6p2_dessireochoa_22111211/Usuarios.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6p2_dessireochoa_22111211;
+
+/**
+ *
+ * @author dessi
+ */
+public class Usuarios {
+    
+}
